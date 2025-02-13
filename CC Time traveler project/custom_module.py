fc2189b5ba49be08fcbfc8f663e916d9c8122271
Markcus <Markcusrichardson3@gmail.com>
2025-02-13 17:19:18 +0000

    Working on week 2 computational intelligence with the sample NNas. Understanding these and started writing my frst one

---
+++ CC Time traveler project/custom_module.py
@@ -0,0 +1,4 @@
+def generate_time_travel_message(year, destination, cost)
+    return "We wnt back to the year " + {year} + " and took the horse and carriage to " + {destination} + " howver this did cost " + {cost}
+    
+    
